d869843102623518269501f0c385d161172a98b2
Sumukhvanjre04 <sumukh0408@gmail.com>
2026-07-25 08:53:22 +0530

    Delete Data Structures & Algorithms/duplicate-integer/submission-1.py

--- Data Structures & Algorithms/duplicate-integer/submission-1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        set_nums=set(nums)
-        if len(nums)!=len(set_nums):
-            return True
-        else:
-            return False
-        
